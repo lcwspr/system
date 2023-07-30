6ed77f22a3a8a42a322285605d808e55ed277c6d
lcwspr <lcwspr@163.com>
2023-07-30 17:13:39 +0000

    feat: 支持保护模式打印

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -12,12 +12,31 @@ mov ss, ax
 mov sp, 0x7c00
 
 ; 0xb800 文本显示器的内存区域
-mov ax, 0xb800
-mov ds, ax
-mov byte [0], 'H'
+; mov ax, 0xb800
+; mov ds, ax
+; mov byte [0], 'H'
+
+xchg bx, bx ; 魔术断点
+mov si, booting
+call print
 
 ;阻塞
 jmp $
+
+print:
+    mov ah, 0x0e
+.next:
+    mov al, [si]
+    cmp al, 0
+    jz .done
+    int 0x10
+    inc si
+    jmp .next
+.done:
+    ret
+
+booting:
+    db "Booting os...", 10, 13, 0
 
 ; 填充 0
 times 510 - ($ - $$) db 0
