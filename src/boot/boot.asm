bedec4e952eac0acad7004384423509103f1934e
lcwspr <lcwspr@163.com>
2023-07-30 17:46:06 +0000

    feat: 读写硬盘

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -20,8 +20,87 @@ xchg bx, bx ; 魔术断点
 mov si, booting
 call print
 
+mov edi, 0x1000 ; 读取的目标内存
+mov ecx, 0  ; 起始扇区
+mov bl, 1   ; 扇区数量
+call read_disk
+
 ;阻塞
 jmp $
+
+read_disk:
+
+    ; 设置读写扇区的数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx      ; 0x1f3
+    mov al, cl  ; 起始扇区的前8位
+    out dx, al
+
+    inc dx      ; 0x1f4
+    shr ecx, 8
+    mov al, cl  ; 起始扇区的中8位
+    out dx, al
+
+    
+    inc dx      ; 0x1f5
+    shr ecx, 8
+    mov al, cl  ; 起始扇区的中8位
+    out dx, al
+
+    inc dx      ; 0x1f6
+    shr ecx, 8
+    and cl, 0b1111  ;高四位置零
+    
+    mov al, 0b1110_0000;
+    or al, cl
+    out dx, al      ; 主盘 LBA 模式
+
+    inc dx     ; 0x1f7
+    mov al, 0x20    ; 读硬盘
+    out dx, al
+
+    ; 清空
+    xor ecx, ecx;
+    ; mov ecx, 0  上面好
+
+    mov cl, bl  ; 得到读写扇区的数量
+
+    .read:
+        push cx             ; 保存 cx
+        call .waits
+        call reads          ; 读取一个扇区
+        pop cx
+        loop .read
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx
+            jmp $+2         ; 直接调整下一行 nop 
+            jmp $+2
+            jmp $+2
+
+            and al, 0b1000_1000
+            cmp al, 0b1000_1000
+            jnz .check
+        ret
+    
+    .reads:
+        mov dx, 0x1f0
+        mov cx, 256         ; 一个扇区 256
+        .readw
+            in ax, dx
+            jmp $+2         ; 直接调整下一行 nop 
+            jmp $+2         ; 直接调整下一行 nop 
+            jmp $+2         ; 直接调整下一行 nop 
+            mov [edi], ax
+            add edi, 2
+            loop .readw
+        ret
 
 print:
     mov ah, 0x0e
